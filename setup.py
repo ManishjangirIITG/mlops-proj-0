34c1fd6aebc23f140adee23dfe340963feecc9fc
ManishjangirIITG <m.jangir@iitg.ac.in>
2026-02-15 20:12:10 +0000

    project structure and env set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Manish Jangir",
+    author_email="work.manishjangiriitg@gmail.com",
+    packages=find_packages()
+)
